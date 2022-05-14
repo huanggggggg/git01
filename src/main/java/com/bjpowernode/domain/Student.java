1d91076b78cf6634713bed8b860f28103942662c
huanggggggg <102397366+huanggggggg@users.noreply.github.com>
2022-05-14 10:46:07 +0800

    huang commit

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -5,4 +5,27 @@ public class Student {
     private String id;
     private Integer age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
